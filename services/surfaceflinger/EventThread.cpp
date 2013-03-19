073d3ef367ba955112797e3a4f2332c790e677d1
oiuyz2008 <oiuyz2008@gmail.com>
2013-03-19 14:56:08 +0800

    Revert "surfaceflinger: Remove deadlock condition."
    This reverts commit 0b47748c73ae8d5e4ff6d5dd3b883096ba8db5f9.

--- services/surfaceflinger/EventThread.cpp
+++ services/surfaceflinger/EventThread.cpp
@@ -73,7 +73,7 @@ status_t EventThread::unregisterDisplayEventConnection(
 }
 
 void EventThread::removeDisplayEventConnection(
-        const sp<EventThread::Connection>& connection) {
+        const wp<EventThread::Connection>& connection) {
     Mutex::Autolock _l(mLock);
     mDisplayEventConnections.remove(connection);
 }
@@ -128,7 +128,7 @@ bool EventThread::threadLoop() {
 
     nsecs_t timestamp;
     DisplayEventReceiver::Event vsync;
-    Vector< sp<EventThread::Connection> > displayEventConnections;
+    Vector< wp<EventThread::Connection> > displayEventConnections;
 
     do {
         Mutex::Autolock _l(mLock);
@@ -222,7 +222,7 @@ bool EventThread::threadLoop() {
 
     const size_t count = displayEventConnections.size();
     for (size_t i=0 ; i<count ; i++) {
-        sp<Connection> conn(displayEventConnections[i]);
+        sp<Connection> conn(displayEventConnections[i].promote());
         // make sure the connection didn't die
         if (conn != NULL) {
             status_t err = conn->postEvent(vsync);
